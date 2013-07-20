0ccc43d6de0ca18f79be45a3a3fced375decdbd8
Mikolaj Habryn <dichro@rcpt.to>
2013-07-20 17:44:27 -0400

    Add an ImageView widget, fail to render into it with UI error.

--- src/to/rcpt/quest/IngestPhotoActivity.java
+++ src/to/rcpt/quest/IngestPhotoActivity.java
@@ -1,47 +1,78 @@
 package to.rcpt.quest;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class IngestPhotoActivity extends Activity {
-
-    private static final String TAG = "IngestPhotoActivity";
+	private static final String TAG = "IngestPhotoActivity";
+	private View imageView;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ingest_photo);
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ingest_photo);
+		imageView = findViewById(R.id.imageView);
+	}
 
 	private class ImageHandler extends AsyncTask<Uri, Integer, Integer> {
 		@Override
-		protected Integer doInBackground(Uri... uri) {
-			Log.i(TAG, "Got " + uri[0]);			
+		protected Integer doInBackground(Uri... uris) {
+			Uri uri = uris[0];
+			Log.i(TAG, "Got " + uri);
+			// TODO(dichro): handle things other than "content://"
+
+			BitmapFactory.Options o = new BitmapFactory.Options();
+			o.inJustDecodeBounds = true;
+
+			String path = uri.getPath();
+			BitmapFactory.decodeFile(path, o);
+			Log.i(TAG, path + ": " + o.outWidth + "x" + o.outHeight);
+			if (o.outWidth == 0 || o.outHeight == 0) {
+				toast("Failed to load image", Toast.LENGTH_SHORT);
+				return 1;
+			}
 			return 0;
-		}		
+		}
 	}
-	
-    @Override
-    protected void onResume() {
-    	super.onResume();
-        Intent intent = getIntent();
-        Log.i(TAG, "resume " + intent);
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Intent intent = getIntent();
+		Log.i(TAG, "resume " + intent);
 		if (Intent.ACTION_SEND.equals(intent.getAction())) {
-        	Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-        	new ImageHandler().execute(uri);
-        }
+			Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+			new ImageHandler().execute(uri);
+		}
 	}
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_ingest_photo, menu);
-        return true;
-    }
-    
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_ingest_photo, menu);
+		return true;
+	}
+
+	private final Handler toaster = new Handler() {
+		// TODO(dichro): resolve this weakreference stuff
+		@Override
+		public void handleMessage(Message msg) {
+			Toast.makeText(IngestPhotoActivity.this, (String) msg.obj, msg.what)
+					.show();
+		}
+	};
+
+	private void toast(String msg, int duration) {
+		toaster.sendMessage(toaster.obtainMessage(duration, msg));
+	}
 }
