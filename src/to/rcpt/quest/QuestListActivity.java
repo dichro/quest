de15ef4fbdb92bd246885cb5e963dfe735fc8ed2
Mikolaj Habryn <dichro@rcpt.to>
2013-08-18 16:36:17 -0400

    render clickable images in list

--- src/to/rcpt/quest/QuestListActivity.java
+++ src/to/rcpt/quest/QuestListActivity.java
@@ -6,20 +6,30 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.View;
 
 public class QuestListActivity extends ListActivity implements
 		LoaderManager.LoaderCallbacks<Cursor> {
 	private SimpleCursorAdapter adapter;
+	private Toaster toast;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		toast = new Toaster(this);
 		getLoaderManager().initLoader(0, null, this);
-		adapter = new SimpleCursorAdapter(this,
-				android.R.layout.simple_list_item_2, null, new String[] {
-						Metadata.Images.ORIGINAL, Metadata.Images.LINEARIZED },
-				new int[] { android.R.id.text1, android.R.id.text2 }, 0);
+		adapter = new SimpleCursorAdapter(this, R.layout.list_item_querylist,
+				null, new String[] {
+						// Metadata.Images.ORIGINAL,
+						Metadata.Images.LINEARIZED, Metadata.Images.SOLUTION,
+						Metadata.Images.CLUE }, new int[] {
+						// R.id.original,
+						R.id.linearized, R.id.solution, R.id.clue }, 0);
 		setListAdapter(adapter);
+	}
+
+	public void editImage(View v) {
+		toast.s("editImage " + v.getId());
 	}
 
 	@Override
